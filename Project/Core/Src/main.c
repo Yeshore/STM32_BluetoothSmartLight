3b118b055bb2d1bd40da9bb382e7c7187c775773
LiFZ <lifzcn@qq.com>
2023-05-08 23:20:12 +0800

    Update some files

--- Project/Core/Src/main.c
+++ Project/Core/Src/main.c
@@ -1,20 +1,20 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.c
-  * @brief          : Main program body
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
@@ -65,17 +65,20 @@ void SystemClock_Config(void);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-	uint8_t x = 0;
-	uint8_t y = 0;
-	uint8_t i = 0;
-	uint8_t dataValue[2] = {0};
-	float luxValue = 0;
+  uint8_t x = 0;
+  uint8_t y = 0;
+  uint8_t i = 0;
+  uint8_t dataValue[2] = {0};
+  float luxValue = 0;
+  uint16_t pwmDuty = 0;
+	uint8_t rxBuffer[1] = {0};
+	uint8_t charCmd = '0';
   /* USER CODE END 1 */
 
   /* MCU Configuration--------------------------------------------------------*/
@@ -102,64 +105,144 @@ int main(void)
   MX_TIM2_Init();
   MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
-	OLED_Init();
-	OLED_Clear();
-	OLED_ShowChinese(x + 24 + 16 * 0, y + 2 * 0, 0);
-	OLED_ShowChinese(x + 24 + 16 * 1, y + 2 * 0, 1);
-	OLED_ShowChinese(x + 24 + 16 * 2, y + 2 * 0, 2);
-	OLED_ShowChinese(x + 24 + 16 * 3, y + 2 * 0, 3);
-	OLED_ShowChinese(x + 24 + 16 * 4, y + 2 * 0, 4);
-	OLED_ShowChinese(x + 16 * 0, y + 2 * 2, 5);
-	OLED_ShowChinese(x + 16 * 1, y + 2 * 2, 6);
-	OLED_ShowChar(x + 16 * 2, y + 2 * 2, ':', 16);
-	OLED_ShowChinese(x + 16 * 0, y + 2 * 3, 11);
-	OLED_ShowChinese(x + 16 * 1, y + 2 * 3, 12);
-	OLED_ShowChinese(x + 16 * 2, y + 2 * 3, 13);
-	OLED_ShowChar(x + 16 * 3, y + 2 * 3, ':', 16);
-	HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
+  OLED_Init();
+  OLED_Clear();
+  OLED_ShowChinese(x + 24 + 16 * 0, y + 2 * 0, 0);
+  OLED_ShowChinese(x + 24 + 16 * 1, y + 2 * 0, 1);
+  OLED_ShowChinese(x + 24 + 16 * 2, y + 2 * 0, 2);
+  OLED_ShowChinese(x + 24 + 16 * 3, y + 2 * 0, 3);
+  OLED_ShowChinese(x + 24 + 16 * 4, y + 2 * 0, 4);
+  OLED_ShowChinese(x + 16 * 0, y + 2 * 2, 5);
+  OLED_ShowChinese(x + 16 * 1, y + 2 * 2, 6);
+  OLED_ShowChar(x + 16 * 2, y + 2 * 2, ':', 16);
+  OLED_ShowChinese(x + 16 * 0, y + 2 * 3, 11);
+  OLED_ShowChinese(x + 16 * 1, y + 2 * 3, 12);
+  OLED_ShowChinese(x + 16 * 2, y + 2 * 3, 13);
+  OLED_ShowChar(x + 16 * 3, y + 2 * 3, ':', 16);
+  HAL_TIM_PWM_Start(&htim2, TIM_CHANNEL_1);
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-		if (PushdownKey_1)
-		{
-			HAL_Delay(50);
-			i += 1;
-		}
+		HAL_UART_Receive_DMA(&huart1, rxBuffer, 1);
+		charCmd = rxBuffer[0];
 		
-		switch(i%2)
-		{
-			case 0:
-				OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 7);
-			  OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 8);
-			break;
-			case 1:
-				OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 9);
-			  OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 10);
-			break;
-			default:
-				OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 7);
-			  OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 8);
-			break;
-		}
+    if (PushdownKey_1)
+    {
+      HAL_Delay(50);
+      i += 1;
+    }
+
+    switch (i % 2)
+    {
+    case 0:
+      OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 7);
+      OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 8);
+		  if (luxValue >= 0 && luxValue <= 2500)
+			{
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, luxValue);
+			}
+			else
+			{
+				;
+			}
+      break;
+    case 1:
+      OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 9);
+      OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 10);
+		  if (PushdownKey_2)
+      {
+        HAL_Delay(50);
+        if (pwmDuty >= 0 && pwmDuty <= 2500)
+        {
+          pwmDuty += 500;
+        }
+        else
+        {
+          ;
+        }
+      }
+      else if (PushdownKey_3)
+      {
+        HAL_Delay(50);
+        if (pwmDuty >= 0 && pwmDuty <= 2500)
+        {
+          pwmDuty -= 500;
+        }
+        else
+        {
+          ;
+        }
+      }
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmDuty);
+      break;
+    default:
+      OLED_ShowChinese(x + 16 * 3, y + 2 * 2, 7);
+      OLED_ShowChinese(x + 16 * 4, y + 2 * 2, 8);
+      break;
+    }
+
+    if (BH1750_SendCmd(ONCE_H_MODE) == HAL_OK)
+    {
+      ;
+    }
+    HAL_Delay(200);
+    if (BH1750_ReadData(dataValue) == HAL_OK)
+    {
+      luxValue = BH1750_GetLux(dataValue);
+    }
+    OLED_ShowNum(x + 16 * 4, y + 2 * 3, luxValue, 4, 16);
+    OLED_ShowChar(x + 16 * 6 + 8 * 1, y + 2 * 3, 'L', 16);
+    OLED_ShowChar(x + 16 * 6 + 8 * 2, y + 2 * 3, 'u', 16);
+    OLED_ShowChar(x + 16 * 6 + 8 * 3, y + 2 * 3, 'x', 16);
 		
-		if (BH1750_SendCmd(ONCE_H_MODE) == HAL_OK)
+		if (HAL_GPIO_ReadPin(HCSR505_IO_GPIO_Port, HCSR505_IO_Pin) == GPIO_PIN_SET)
+		{
+			__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 2500);
+		}
+		else
 		{
 			;
 		}
-		HAL_Delay(200);
-		if (BH1750_ReadData(dataValue) == HAL_OK)
-		{
-			luxValue = BH1750_GetLux(dataValue);
-		}
-		OLED_ShowNum(x + 16 * 4, y + 2 * 3, luxValue, 4, 16);
-		OLED_ShowChar(x + 16 * 6 + 8 * 1, y + 2 * 3, 'L', 16);
-		OLED_ShowChar(x + 16 * 6 + 8 * 2, y + 2 * 3, 'u', 16);
-		OLED_ShowChar(x + 16 * 6 + 8 * 3, y + 2 * 3, 'x', 16);
 		
-		HAL_Delay(1000);
+		switch(charCmd)
+		{
+			case '0':
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 0);
+			break;
+			case '1':
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, 2500);
+			break;
+			case '3':
+				if (pwmDuty >= 0 && pwmDuty <= 2500)
+        {
+          pwmDuty += 500;
+        }
+        else
+        {
+          ;
+        }
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmDuty);
+			break;
+			case '4':
+				if (pwmDuty >= 0 && pwmDuty <= 2500)
+        {
+          pwmDuty -= 500;
+        }
+        else
+        {
+          ;
+        }
+				__HAL_TIM_SET_COMPARE(&htim2, TIM_CHANNEL_1, pwmDuty);
+			break;
+			default:
+				;
+			break;
+		}
+		
+    HAL_Delay(1000);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -168,17 +251,17 @@ int main(void)
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
   RCC_OscInitStruct.HSEState = RCC_HSE_ON;
   RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
@@ -192,9 +275,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
@@ -211,9 +293,9 @@ void SystemClock_Config(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
@@ -225,14 +307,14 @@ void Error_Handler(void)
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
